1919c7440498dd5b98e75c94b24df356ab19dca2
NasikhCL <nasikcl@gmail.com>
2022-06-14 21:17:54 +0530

    multiple routers and controllers are sdded

--- routers/index.js
+++ routers/index.js
@@ -2,14 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const homeController =require('../controllers/home_controller');
-const usersController = require('../controllers/users_controller');
+
 
 
 console.log('router is loaded');
 
+router.get('/', homeController.home);
 
-router.get('/', homeController.home)
-
-router.get('/profile',usersController.profile);
+router.use('/users',require('./users'));
 
 module.exports = router;
