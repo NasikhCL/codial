c96cb0c80cc54d3b698ca31d871423a808d95ccc
NasikhCL <nasikcl@gmail.com>
2022-07-01 18:53:08 +0530

    passport authentication check is added to person who posts a post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,20 @@ const Post = require('../models/post')
 module.exports.home = function(req,res) {
 // console.log(req.cookies);
 // res.cookie('somthing', 'blah')
-    Post.find({},function(err,posts){
+ 
+
+// Post.find({},function(err,posts){
+    //     if(err){console.log("erro in loading posts"); return;}
+    //     return res.render('home',{
+    //         title: "codia | home",
+    //         posts: posts
+    //     });
+
+    // })
+
+    // poplate the user of each post 
+
+    Post.find({}).populate('user').exec(function(err,posts){
         if(err){console.log("erro in loading posts"); return;}
         return res.render('home',{
             title: "codia | home",
